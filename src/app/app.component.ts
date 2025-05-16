fe876cb0ceb1b04c771258287bb43d1d0a9545e7
buckapi <buckapis@gmail.com>
2025-05-16 14:44:17 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { SidebarComponent } from './components/ui/sidebar/sidebar.component';
+import { MenubarComponent } from './components/ui/menubar/menubar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, MenubarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
